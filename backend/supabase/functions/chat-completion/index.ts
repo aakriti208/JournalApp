d612e25a745c52792e820923cafb941b7beea69b
aakriti208 <aakritidh208@gmail.com>
2025-10-25 12:44:39 -0500

    Validation on response

--- backend/supabase/functions/chat-completion/index.ts
+++ backend/supabase/functions/chat-completion/index.ts
@@ -1,1 +1,30 @@
-import {serve}
+// import {serve}
+
+export async function handler(req){
+    const url = new URL(req.url);
+
+    //query parameter
+    const notify = url.searchParams.get("notify");
+
+    //Path parameter
+    const userId = url.pathname.split("/").pop();
+
+    //Header
+    const token = req.headers.get("Authorization");
+
+    //Body
+    // const body = await req.json();
+    // const {name, age} = body;
+
+    // Validation on a request
+    const body = await req.json();
+    if(!body.name || typeof body.name != "string") {
+        return new Response("Invalid name", {status : 400});
+    }
+    if(!body.age || typeof body.age !== "number") {
+        return new Response("Invalid age", {status : 400});
+    }
+}
+
+
+
